2512e8cf0b4747c7ebc51a0413f275d292ac6a46
whyye <whyye@126.com>
2019-12-31 16:12:38 +0800

    修改
    项目初始化修改

---
+++ admin/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../views/Main.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  }
+  // {
+  //   path: '/',
+  //   name: 'Main',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
